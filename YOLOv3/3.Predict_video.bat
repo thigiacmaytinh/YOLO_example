9348aeb74784201ee7b97d8a001890a393194bee
vohungvi <vohungvi27@gmail.com>
2021-06-04 19:25:52 +0700

    - predict video

---
+++ YOLOv3/3.Predict_video.bat
@@ -0,0 +1,2 @@
+python yolov3.py --config ./yolov3.cfg --weights yolov3.weights --classes yolov3.names --video VID_20210509_174515.mp4
+pause
--- YOLOv3/YOLOv3.py
+++ YOLOv3/YOLOv3.py
@@ -6,15 +6,22 @@ import argparse
 import numpy as np
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True,
-                help='path to input image')
-ap.add_argument('-c', '--config', required=True,
-                help='path to yolo config file')
-ap.add_argument('-w', '--weights', required=True,
-                help='path to yolo pre-trained weights')
-ap.add_argument('-cl', '--classes', required=True,
-                help='path to text file containing class names')
+ap.add_argument('-i', '--image', required=False, help='path to input image')
+ap.add_argument('-v', '--video', required=False, help='path to input video')
+ap.add_argument('-c', '--config', required=True, help='path to yolo config file')
+ap.add_argument('-w', '--weights', required=True, help='path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True, help='path to text file containing class names')
+ap.add_argument('-co', '--confidence', type=float, default=0.5, help='path to text file containing class names')
+ap.add_argument('-th', '--threshold', type=float, default=0.3, help='path to text file containing class names')
+
 args = ap.parse_args()
+FLAGS, unparsed = ap.parse_known_args()
+
+classes = None
+with open(args.classes, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
 ###################################################################################################
 
@@ -38,71 +45,216 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
 ###################################################################################################
 
-image = cv2.imread(args.image)
+def generate_boxes_confidences_classids(outs, height, width, tconf):
+    boxes = []
+    confidences = []
+    classids = []
 
-Width = image.shape[1]
-Height = image.shape[0]
-scale = 0.00392
+    for out in outs:
+        for detection in out:
+            #print (detection)
+            #a = input('GO!')
+            
+            # Get the scores, classid, and the confidence of the prediction
+            scores = detection[5:]
+            classid = np.argmax(scores)
+            confidence = scores[classid]
+            
+            # Consider only the predictions that are above a certain confidence level
+            if confidence > tconf:
+                # TODO Check detection
+                box = detection[0:4] * np.array([width, height, width, height])
+                centerX, centerY, bwidth, bheight = box.astype('int')
 
-classes = None
+                # Using the center x, y coordinates to derive the top
+                # and the left corner of the bounding box
+                x = int(centerX - (bwidth / 2))
+                y = int(centerY - (bheight / 2))
 
-with open(args.classes, 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
+                # Append to list
+                boxes.append([x, y, int(bwidth), int(bheight)])
+                confidences.append(float(confidence))
+                classids.append(classid)
 
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    return boxes, confidences, classids
 
-net = cv2.dnn.readNet(args.weights, args.config)
+###################################################################################################
 
-blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+def draw_labels_and_boxes(img, boxes, confidences, classids, idxs, colors, labels):
+    # If there are any detections
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            # Get the bounding box coordinates
+            x, y = boxes[i][0], boxes[i][1]
+            w, h = boxes[i][2], boxes[i][3]
+            
+            # Get the unique color for this class
+            color = [int(c) for c in colors[classids[i]]]
 
-net.setInput(blob)
+            # Draw the bounding box rectangle and label on the image
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+            text = "{}: {:4f}".format(labels[classids[i]], confidences[i])
+            cv2.putText(img, text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-outs = net.forward(get_output_layers(net))
+    return img
 
-class_ids = []
-confidences = []
-boxes = []
-conf_threshold = 0.5
-nms_threshold = 0.4
+###################################################################################################
 
-# Thực hiện xác định bằng HOG và SVM
-start = time.time()
+def infer_image(net, layer_names, height, width, img, colors, labels, FLAGS, 
+            boxes=None, confidences=None, classids=None, idxs=None, infer=True):
+    
+    if infer:
+        # Contructing a blob from the input image
+        blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            center_x = int(detection[0] * Width)
-            center_y = int(detection[1] * Height)
-            w = int(detection[2] * Width)
-            h = int(detection[3] * Height)
-            x = center_x - w / 2
-            y = center_y - h / 2
-            class_ids.append(class_id)
-            confidences.append(float(confidence))
-            boxes.append([x, y, w, h])
+        # Perform a forward pass of the YOLO object detector
+        net.setInput(blob)
 
-indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+        # Getting the outputs from the output layers
+        start = time.time()
+        outs = net.forward(layer_names)
+        end = time.time()
 
-for i in indices:
-    i = i[0]
-    box = boxes[i]
-    x = round(box[0])
-    y = round(box[1])
-    w = round(box[2])
-    h = round(box[3])
-    draw_prediction(image, class_ids[i], confidences[i], x, y, x + w, y + h)
+        show_time = True
+        if show_time:
+            print ("[INFO] YOLOv3 took {:6f} seconds".format(end - start))
 
-cv2.imshow("Predict result", image)
+        
+        # Generate the boxes, confidences, and classIDs
+        boxes, confidences, classids = generate_boxes_confidences_classids(outs, height, width, FLAGS.confidence)
+        
+        # Apply Non-Maxima Suppression to suppress overlapping bounding boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, FLAGS.confidence, FLAGS.threshold)
 
+    if boxes is None or confidences is None or idxs is None or classids is None:
+        raise '[ERROR] Required variables are set to None before drawing boxes on images.'
+        
+    # Draw labels and boxes on the image
+    img = draw_labels_and_boxes(img, boxes, confidences, classids, idxs, colors, labels)
 
-end = time.time()
-print("YOLO Execution elapsed time: " + str(end-start))
+    return img, boxes, confidences, classids, idxs
 
 
-cv2.waitKey()
+###################################################################################################
 
-cv2.imwrite("result.jpg", image)
-cv2.destroyAllWindows()
+def ProcessVideo(videoPath):    
+    try:
+        vid = cv2.VideoCapture(videoPath)
+        height, width = None, None
+        writer = None
+    except:
+        raise 'Video cannot be loaded!\n\
+                            Please check the path provided!'
+
+    finally:
+        # Load the weights and configutation to form the pretrained YOLOv3 model
+        net = cv2.dnn.readNetFromDarknet(args.config, args.weights)
+
+        # Get the output layer names of the model
+        layer_names = net.getLayerNames()
+        layer_names = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+        totalFrame = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        frameIdx = 1
+
+        # Intializing colors to represent each label uniquely
+        colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8')
+
+        while True:
+            grabbed, frame = vid.read()
+
+            # Checking if the complete video is read
+            if not grabbed:
+                break
+
+            if width is None or height is None:
+                height, width = frame.shape[:2]
+
+            frame, _, _, _, _ = infer_image(net, layer_names, height, width, frame, colors, classes, FLAGS)
+
+            if(writer == None):
+                # Initialize the video writer
+                fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+                writer = cv2.VideoWriter("result.avi", fourcc, 30, (frame.shape[1], frame.shape[0]), True)
+
+            writer.write(frame)
+            print(str(frameIdx) + "/" + str(totalFrame))
+            frameIdx+=1
+
+        print ("[INFO] Cleaning up...")
+        writer.release()
+        vid.release()
+
+###################################################################################################
+
+def ProcessImage(image):
+    width = image.shape[1]
+    height = image.shape[0]
+    scale = 0.00392
+
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    net = cv2.dnn.readNet(args.weights, args.config)
+
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    # Thực hiện xác định bằng HOG và SVM   
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = round(box[0])
+        y = round(box[1])
+        w = round(box[2])
+        h = round(box[3])
+        draw_prediction(image, class_ids[i], confidences[i], x, y, x + w, y + h)
+    
+    return image
+    
+
+###################################################################################################
+
+
+if __name__ == '__main__':    
+    start = time.time()
+    if(args.image == None and args.video == None):
+        print("Missing --image or --video")
+    elif(args.image != None):
+        image = cv2.imread(args.image)     
+        image = ProcessImage(image)
+
+        cv2.imshow("Predict result", image)    
+        cv2.imwrite("result.jpg", image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    elif(args.video != None):       
+        ProcessVideo(args.video)
+        end = time.time()
+        print("YOLO Execution elapsed time: " + str(end-start))
